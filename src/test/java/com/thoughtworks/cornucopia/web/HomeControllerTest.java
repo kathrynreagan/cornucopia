67d55b5d04731dca4c7adce2d4e352bdc6d74ffc
jace45 <jamestephens09@gmail.com>
2016-08-04 15:14:12 -0500

    [Ella/Jayson][#1] added tests in HomeControllerTest

--- src/test/java/com/thoughtworks/cornucopia/web/HomeControllerTest.java
+++ src/test/java/com/thoughtworks/cornucopia/web/HomeControllerTest.java
@@ -1,4 +1,18 @@
 package com.thoughtworks.cornucopia.web;
 
+import com.thoughtworks.cornucopia.HomeController;
+import org.junit.Test;
+import org.springframework.ui.ModelMap;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class HomeControllerTest {
+
+    @Test
+    public void testPrintWelcomeReturnsHome() {
+        HomeController homeController = new HomeController();
+        ModelMap modelMap = new ModelMap();
+
+        assertEquals("home", homeController.printWelcome(modelMap));
+     }
 }
